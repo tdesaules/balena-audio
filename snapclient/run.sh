a1f81d202d32ea912859f215843a6652e968c0a0
Thibault DESAULES <thibault.desaules@sodexo.com>
2021-05-03 18:34:26 +0200

    remove jq issue

--- snapclient/run.sh
+++ snapclient/run.sh
@@ -2,7 +2,7 @@
 
 # if tag to be stop then exit it
 if [ "$IS_STARTED" != "true" ] ; then
-    curl --silent --request POST --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/stop-service?apikey=$BALENA_SUPERVISOR_API_KEY" --data '{"serviceName": "'$BALENA_SERVICE_NAME'"}' | jq
+    curl --silent --request POST --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/stop-service?apikey=$BALENA_SUPERVISOR_API_KEY" --data '{"serviceName": "'$BALENA_SERVICE_NAME'"}'
     sleep 15
 fi
 
